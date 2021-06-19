ee193d0a98532a2ffdf48bba0bfdbfbbb93ba9c7
Maxime Daraize <leeroydevassistant2@gmail.com>
2021-06-19 08:22:41 -0400

    Feat: add alias for './src' path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+const path = require('path')
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    // create an alias '/@' for the './src' path
+    resolve: {
+        alias: {
+            '/@': path.resolve(__dirname, './src'),
+        },
+    },
+    plugins: [vue()],
 })
