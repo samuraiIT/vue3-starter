e81767a8f0ae817b5946e6a33a3e2c601f5f2d22
Maxime Daraize <leeroydevassistant2@gmail.com>
2021-06-19 07:01:39 -0400

    Feat: Add 'active' & 'visited' variant to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,17 @@ Optimizing for Production Guide:
 <https://tailwindcss.com/docs/optimizing-for-production>
 */
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js}"],
+  purge: ["./index.html", "./src/**/*.{vue,js}"], // purge class from index.html and all vue.js files
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      // add interactions states not present by default in the tailwind toolbelt
+      backgroundColor: ["active"],
+      textColor: ["visited"],
+    },
   },
   plugins: [],
 };
