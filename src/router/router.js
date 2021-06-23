686c5a9fbb63d90906283b26331e1edad3b7835f
Maxime Daraize <leeroydevassistant2@gmail.com>
2021-06-23 16:51:24 -0400

    Clean: Refactor, remove some unused import

--- src/router/router.js
+++ src/router/router.js
@@ -1,48 +1,45 @@
-import { createRouter, createWebHistory } from "vue-router";
-import store from "../store";
-import i18n from "/@/i18n/i18n";
+import { createRouter, createWebHistory } from 'vue-router'
+import i18n from '/@/i18n/i18n'
 
-const history = createWebHistory();
+const history = createWebHistory()
 const router = createRouter({
-  history,
-  routes: [
-    {
-      path: "/",
-      name: "Home-en",
-      component: () => import("/@/views/Home.vue"),
-      meta: { lang: "en" },
-    },
-    {
-      path: "/fr",
-      name: "Home-fr",
-      component: () => import("/@/views/Home.vue"),
-      meta: { lang: "fr" },
-    },
-    {
-      path: "/about",
-      name: "About-en",
-      meta: { lang: "en" },
-      component: () => import("/@/views/About.vue"),
-    },
-    {
-      path: "/a-propos",
-      name: "About-fr",
-      meta: { lang: "fr" },
-      component: () => import("/@/views/About.vue"),
-    },
-  ],
-});
+    history,
+    routes: [
+        {
+            path: '/',
+            name: 'Home-en',
+            component: () => import('/@/views/Home.vue'),
+            meta: { lang: 'en' },
+        },
+        {
+            path: '/fr',
+            name: 'Home-fr',
+            component: () => import('/@/views/Home.vue'),
+            meta: { lang: 'fr' },
+        },
+        {
+            path: '/documentation',
+            name: 'Doc-en',
+            meta: { lang: 'en' },
+            component: () => import('/@/views/Doc.vue'),
+        },
+        {
+            path: '/fr/documentation',
+            name: 'Doc-fr',
+            meta: { lang: 'fr' },
+            component: () => import('/@/views/Doc.vue'),
+        },
+    ],
+})
 
 /**
  * After each route change, we set the lang
  * we also set the siteId to query the correct lang
  */
-router.afterEach((to) => {
-  if (to.meta.lang) {
-    store.dispatch("ui/setLang", to.meta.lang);
-    store.dispatch("ui/setSiteId", to.meta.siteId);
-    i18n.global.locale = to.meta.lang;
-  }
-});
+router.afterEach(to => {
+    if (to.meta.lang) {
+        i18n.global.locale = to.meta.lang
+    }
+})
 
-export default router;
+export default router
