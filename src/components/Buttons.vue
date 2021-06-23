a30d2be760ef946893b900b1c9618d06ccaae935
Maxime Daraize <leeroydevassistant2@gmail.com>
2021-06-23 17:34:26 -0400

    Clean: indentation fix

--- src/components/Buttons.vue
+++ src/components/Buttons.vue
@@ -1,52 +1,52 @@
 <template>
-  <h1 class="text-4xl mb-3">{{ msg }}</h1>
-  <button @click="increment()" class="button">Increase total by 1</button>
-  <button @click="decrement()" class="button" style="background: #ffcd2a">
-    Decrease total by 1
-  </button>
-  <br />
-  <span class="count">Total: {{ count }}</span>
+    <h1 class="text-4xl mb-3">{{ msg }}</h1>
+    <button @click="increment()" class="button">Increase total by 1</button>
+    <button @click="decrement()" class="button" style="background: #ffcd2a">
+        Decrease total by 1
+    </button>
+    <br />
+    <span class="count">Total: {{ count }}</span>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref } from 'vue'
 
 export default {
-  props: {
-    msg: "",
-  },
-  setup() {
-    let count = ref(0);
-    const increment = () => {
-      count.value = count.value + 1;
-    };
-    const decrement = () => {
-      count.value = count.value - 1;
-    };
-    return {
-      count,
-      increment,
-      decrement,
-    };
-  },
-};
+    props: {
+        msg: '',
+    },
+    setup() {
+        let count = ref(0)
+        const increment = () => {
+            count.value = count.value + 1
+        }
+        const decrement = () => {
+            count.value = count.value - 1
+        }
+        return {
+            count,
+            increment,
+            decrement,
+        }
+    },
+}
 </script>
 
 <style scoped>
 .button {
-  display: inline-block;
-  border-radius: 6px;
-  padding: 8px 12px;
-  font-size: 1.5rem;
-  font-weight: 700;
-  color: #fff;
-  background: #41b883;
-  margin: 0 8px 2rem;
+    display: inline-block;
+    border-radius: 6px;
+    padding: 8px 12px;
+    font-size: 1.5rem;
+    font-weight: 700;
+    color: #fff;
+    background: #41b883;
+    margin: 0 8px 2rem;
 }
 
 .count {
-  font-size: 1.5rem;
-  color: #2c3e50;
-  margin-left: 8px;
+    font-size: 1.5rem;
+    color: #2c3e50;
+    margin-left: 8px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <h1>{{ $t("home") }}</h1>
-  <Buttons msg="Welcome to Vite & Vue3 Starter" />
+    <h1>{{ $t('home') }}</h1>
+    <Buttons msg="Welcome to Vite & Vue3 Starter" />
 </template>
 
 <script>
-import Buttons from "/@/components/Buttons.vue";
+import Buttons from '/@/components/Buttons.vue'
 export default {
-  components: {
-    Buttons,
-  },
-  setup() {
-    return {};
-  },
-};
+    components: {
+        Buttons,
+    },
+    setup() {
+        return {}
+    },
+}
 </script>
 
 <style scoped></style>
