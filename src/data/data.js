e3446b2ecf06f676d17f8969185b97e95d404b4f
Maxime Daraize <leeroydevassistant2@gmail.com>
2021-06-26 16:03:42 -0400

    Feat: Create static data

---
+++ src/data/data.js
@@ -0,0 +1,13 @@
+export default {
+    en: [
+        { id: 1, title: 'First title', description: 'First description' },
+        { id: 2, title: 'Second title', description: 'Second description' },
+        { id: 3, title: 'Third title', description: 'Third description' },
+    ],
+
+    fr: [
+        { id: 1, title: 'Premier titre', description: 'Premier description' },
+        { id: 2, title: 'Deuxième titre', description: 'Deuxième description' },
+        { id: 3, title: 'Troisième titre', description: 'Troisième description' },
+    ],
+}
