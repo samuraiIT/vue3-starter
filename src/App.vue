ffa9b25625d772d3f7626eacf11cd49a368941d2
Maxime Daraize <leeroydevassistant2@gmail.com>
2021-06-26 16:01:17 -0400

    Style: Add global style in App component

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,15 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+}
+
+main {
+    padding: 3rem;
+    max-width: 900px;
+    margin: 0 auto;
+}
+
+p + p {
+    margin-top: 1.25rem;
 }
 </style>
