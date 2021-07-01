7629e774ffa86f02f1dad79a9d64407686515d02
Maxime Daraize <maxime.daraize@gmail.com>
2021-07-01 09:13:23 -0400

    Feat: Change data for cities

--- src/data/data.js
+++ src/data/data.js
@@ -1,13 +1,87 @@
 export default {
     en: [
-        { id: 1, title: 'First title', description: 'First description' },
-        { id: 2, title: 'Second title', description: 'Second description' },
-        { id: 3, title: 'Third title', description: 'Third description' },
+        {
+            id: 1,
+            slug: 'montreal',
+            city: 'Montreal',
+            description:
+                "Montreal is the largest city in Canada's Québec province. It’s set on an island in the Saint Lawrence River and named after Mt. Royal, the triple-peaked hill at its heart. Its boroughs, many of which were once independent cities, include neighbourhoods ranging from cobblestoned, French colonial Vieux-Montréal – with the Gothic Revival Notre-Dame Basilica at its centre – to bohemian Plateau.",
+            population: '1.78 million',
+            area: '431.5 km²',
+            img: 'https://images.unsplash.com/photo-1559682468-a6a29e7d9517?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+        },
+        {
+            id: 2,
+            slug: 'paris',
+            city: 'Paris',
+            description:
+                "Paris, France's capital, is a major European city and a global center for art, fashion, gastronomy and culture. Its 19th-century cityscape is crisscrossed by wide boulevards and the River Seine. Beyond such landmarks as the Eiffel Tower and the 12th-century, Gothic Notre-Dame cathedral, the city is known for its cafe culture and designer boutiques along the Rue du Faubourg Saint-Honoré.",
+            population: '2.161 million',
+            area: '105.4 km²',
+            img: 'https://images.unsplash.com/photo-1431274172761-fca41d930114?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+        },
+        {
+            id: 3,
+            slug: 'tokyo',
+            city: 'Tokyo',
+            description:
+                "Tokyo, Japan’s busy capital, mixes the ultramodern and the traditional, from neon-lit skyscrapers to historic temples. The opulent Meiji Shinto Shrine is known for its towering gate and surrounding woods. The Imperial Palace sits amid large public gardens. The city's many museums offer exhibits ranging from classical art (in the Tokyo National Museum) to a reconstructed kabuki theater (in the Edo-Tokyo Museum).",
+            population: '13.96 million',
+            area: '2,194 km²',
+            img: 'https://images.unsplash.com/photo-1494587416117-f102a2ac0a8d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+        },
+        {
+            id: 4,
+            slug: 'sydney',
+            city: 'Sydney',
+            description:
+                "Sydney, capital of New South Wales and one of Australia's largest cities, is best known for its harbourfront Sydney Opera House, with a distinctive sail-like design. Massive Darling Harbour and the smaller Circular Quay port are hubs of waterside life, with the arched Harbour Bridge and esteemed Royal Botanic Garden nearby. Sydney Tower’s outdoor platform, the Skywalk, offers 360-degree views of the city and suburbs.",
+            population: '5.312 million',
+            area: '12,368 km²',
+            img: 'https://images.unsplash.com/photo-1524820197278-540916411e20?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1972&q=80',
+        },
     ],
 
     fr: [
-        { id: 1, title: 'Premier titre', description: 'Premier description' },
-        { id: 2, title: 'Deuxième titre', description: 'Deuxième description' },
-        { id: 3, title: 'Troisième titre', description: 'Troisième description' },
+        {
+            id: 1,
+            slug: 'montreal',
+            city: 'Montréal',
+            description:
+                'Montréal est la plus grande ville de la province de Québec au Canada. Il est situé sur une île du fleuve Saint-Laurent et porte le nom du mont. Royal, la colline aux trois sommets en son cœur. Ses arrondissements, dont beaucoup étaient autrefois des villes indépendantes, comprennent des quartiers allant du Vieux-Montréal colonial français pavé – avec la basilique Notre-Dame de style néogothique en son centre – au Plateau bohème.',
+            population: '1.78 million',
+            area: '431.5 km²',
+            img: 'https://images.unsplash.com/photo-1559682468-a6a29e7d9517?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+        },
+        {
+            id: 2,
+            slug: 'paris',
+            city: 'Paris',
+            description:
+                "Paris, capitale de la France, est une grande ville européenne et un centre mondial d'art, de mode, de gastronomie et de culture. Son paysage urbain du XIXe siècle est sillonné de larges boulevards et de la Seine. Au-delà de monuments tels que la Tour Eiffel et la cathédrale gothique Notre-Dame du XIIe siècle, la ville est connue pour sa culture des cafés et ses boutiques de créateurs le long de la rue du Faubourg Saint-Honoré.",
+            population: '2.161 million',
+            area: '105.4 km²',
+            img: 'https://images.unsplash.com/photo-1431274172761-fca41d930114?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+        },
+        {
+            id: 3,
+            slug: 'tokyo',
+            city: 'Tokyo',
+            description:
+                "Tokyo, la capitale animée du Japon, mélange l'ultramoderne et le traditionnel, des gratte-ciel éclairés au néon aux temples historiques. L'opulent sanctuaire shinto Meiji est connu pour sa porte imposante et les bois environnants. Le palais impérial se trouve au milieu de grands jardins publics. Les nombreux musées de la ville proposent des expositions allant de l'art classique (au musée national de Tokyo) à un théâtre kabuki reconstitué (au musée Edo-Tokyo).",
+            population: '13.96 million',
+            area: '2,194 km²',
+            img: 'https://images.unsplash.com/photo-1494587416117-f102a2ac0a8d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+        },
+        {
+            id: 4,
+            slug: 'sydney',
+            city: 'Sydney',
+            description:
+                "Sydney, capitale de la Nouvelle-Galles du Sud et l'une des plus grandes villes d'Australie, est surtout connue pour son opéra de Sydney en front de mer, avec un design distinctif en forme de voile. L'immense port Darling Harbour et le plus petit port de Circular Quay sont des centres de la vie au bord de l'eau, avec le Harbour Bridge voûté et le prestigieux jardin botanique royal à proximité. La plate-forme extérieure de la tour de Sydney, le Skywalk, offre une vue à 360 degrés sur la ville et les banlieues.",
+            population: '5.312 million',
+            area: '12,368 km²',
+            img: 'https://images.unsplash.com/photo-1524820197278-540916411e20?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1972&q=80',
+        },
     ],
 }
