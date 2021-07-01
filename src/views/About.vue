68516dd5c20ad16647642ec3a2e8bd8166a3e241
Maxime Daraize <maxime.daraize@gmail.com>
2021-07-01 09:14:31 -0400

    Feat: add new translation and content for About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,21 @@
 <template>
     <main>
-        <p class="text-left">
-            {{$t('about.text1')}}
+        <h1 class="text-3xl">{{ $t('about.title') }}</h1>
+        <p class="text-left mt-4">
+            {{ $t('about.text') }}
         </p>
 
-        <p class="text-left"> {{$t('about.text2')}}</p>
+        <ul>
+            <li><a :href="$t('about.docs.vite')" target="_blank" rel="noopener">vite</a></li>
+            <li><a :href="$t('about.docs.vuejs')" target="_blank" rel="noopener">vuejs</a></li>
+            <li>
+                <a :href="$t('about.docs.tailwind')" target="_blank" rel="noopener">tailwind</a>
+            </li>
+            <li><a :href="$t('about.docs.vueI18n')" target="_blank" rel="noopener">vue-i18n</a></li>
+            <li>
+                <a :href="$t('about.docs.vueRouter')" target="_blank" rel="noopener">vue-router</a>
+            </li>
+        </ul>
     </main>
 </template>
 
@@ -16,4 +27,22 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+p {
+    font-size: 1.2rem;
+}
+ul {
+    margin-top: 2rem;
+    list-style-type: disc;
+}
+li {
+    text-align: left;
+    font-size: 1.2rem;
+    text-transform: capitalize;
+    margin: 0.5rem 0;
+}
+
+a:hover {
+    text-decoration: underline;
+}
+</style>
