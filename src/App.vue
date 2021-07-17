ed9537460d711e1020e8eaa21295b84d3f47cdd1
Maxime Daraize <maxime.daraize@gmail.com>
2021-07-17 11:23:55 -0400

    feat: create example of scss in the app #minor

--- src/App.vue
+++ src/App.vue
@@ -15,22 +15,5 @@ export default {
 }
 </script>
 
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
-
-main {
-    padding: 3rem;
-    max-width: 900px;
-    margin: 0 auto;
-}
-
-p + p {
-    margin-top: 1.25rem;
-}
+<style lang="scss">
 </style>
