b5ab58d6410d08dc6f0c7586c575b96b75add96f
Maxime Daraize <maxime.daraize@gmail.com>
2021-07-17 10:52:08 -0400

    feat: import configuration for css/scss

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,17 @@ const path = require('path')
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    // create an alias '/@' for the './src' path
     resolve: {
         alias: {
             '/@': path.resolve(__dirname, './src'),
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "./src/assets/scss/main";`,
+            },
         },
     },
     plugins: [vue()],
