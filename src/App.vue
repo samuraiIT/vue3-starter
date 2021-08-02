3d29cfc5964f633628889f17fc9c6356dc1884c8
Maxime Daraize <maxime.daraize@gmail.com>
2021-08-02 16:32:06 -0400

    feat: Add new global styles

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,15 @@ export default {
 </script>
 
 <style lang="scss">
+h1 {
+    font-size: 48px;
+}
+
+h2 {
+    font-size: 32px;
+}
+
+section {
+    border-bottom: 1px solid rgba(#212121, 0.1);
+}
 </style>
