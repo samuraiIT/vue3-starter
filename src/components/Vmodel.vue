a53cb6dc0c984aff1db1fa077deec22d970d26c7
Maxime Daraize <maxime.daraize@gmail.com>
2021-08-02 16:33:34 -0400

    feat: v-model component

---
+++ src/components/Vmodel.vue
@@ -0,0 +1,21 @@
+<template>
+    <input type="text" v-model="firstName" />
+    <p>Hello my name is {{ firstName }}</p>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    setup() {
+        let firstName = ref('')
+        return { firstName }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+input {
+    border: 1px solid #212121;
+    margin-bottom: 8px;
+}
+</style>
